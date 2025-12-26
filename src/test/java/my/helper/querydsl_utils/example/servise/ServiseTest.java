71ce0e44f315c5a952e17b7d98ece87f8a54a515
a.dotsyj <andriy51302@gmail.com>
2025-12-26 23:11:38 +0200

    modify tests

--- src/test/java/my/helper/querydsl_utils/example/servise/ServiseTest.java
+++ src/test/java/my/helper/querydsl_utils/example/servise/ServiseTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class ServiseTest {
 
     @Autowired
